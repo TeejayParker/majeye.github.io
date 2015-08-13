345ff1e56114d36bea8e921195376473a9af349a
Teejay <teejayparker@gmail.com>
2015-08-12 20:20:13 -0500

    FreeCodeCamp PlugDJ settings

---
+++ setting.php
@@ -0,0 +1,43 @@
+{
+    "room": "FreeCodeCamp",
+    "author": "FreeCodeCamp",
+    "icon": null,
+    "css": "https://www.dropbox.com/s/lzj3422632kpxxe/style.css",
+    "rules": {
+        "allowAutorespond": true,
+        "allowAutowoot": true,
+        "allowAutojoin": true,
+        "allowAutograb": true,
+        "allowSmartVote": true,
+        "allowEmotes": true,
+        "allowShowingMehs": true,
+        "forceSmartVote": false
+    },
+    "ccc": {
+        "admin": null,
+        "ambassador": null,
+        "host": null,
+        "cohost": null,
+        "manager": null,
+        "bouncer": null,
+        "rdj": null,
+        "subscriber": null,
+        "friend": null,
+        "user": null
+    },
+    "images": {
+        "background": "http://i.imgur.com/BYs4GEd.jpg",
+        "playback": null,
+        "admin": null,
+        "ambassador": null,
+        "host": null,
+        "cohost": null,
+        "manager": null,
+        "bouncer": null,
+        "rdj": null,
+        "subscriber": null
+    },
+    "emotes": {
+    	"emote_name": null
+    }
+}
