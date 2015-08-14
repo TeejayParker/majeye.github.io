6cf50ef3fc8f19a9f62fb507fc43248b768bff33
Teejay Parker <Majeye@users.noreply.github.com>
2015-08-14 06:11:38 -0500

    Update and rename freecodecamp.css to freecodecamp.php

--- freecodecamp.php
+++ freecodecamp.php
@@ -1,3 +1,8 @@
+<?php 
+header('Access-Control-Allow-Origin: https://plug.dj'); 
+header('Content-Type: text/css'); 
+?>
+
 #chat [data-cid^="4123271-"] i.bdg, #user-rollover.id-4123271 i.bdg, #chat .id-4123271 i.bdg {
     background: url('http://chilloutmusic.fm/img/theme/Icons/3292657.gif') !important;
     background-size: cover;
