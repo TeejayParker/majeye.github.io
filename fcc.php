adf3b6a02ab243fd870531223a26f23199f6e6a8
Teejay Parker <Majeye@users.noreply.github.com>
2015-08-14 06:12:12 -0500

    Update fcc.php

--- fcc.php
+++ fcc.php
@@ -2,7 +2,7 @@
     "room":                 "FreeCodeCamp",
     "author":               "Majeye",
     "icon":                 "http://i.imgur.com/wFTqbRu.png",
-    "css":                  "https://raw.githubusercontent.com/Majeye/majeye.github.io/master/freecodecamp.css",
+    "css":                  "https://raw.githubusercontent.com/Majeye/majeye.github.io/master/freecodecamp.php",
     "rules": {
         "allowAutorespond": true,
         "allowAutowoot":    true,
